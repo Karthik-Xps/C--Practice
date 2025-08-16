b35db21a02584fd003f606ba8ca076bec15be57b
Karthik G <vganterkarthik@gmail.com>
2025-08-16 18:29:50 +0530

    Palindrome Problems

---
+++ C#_Practice Problems/Palindrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Practice_Problems
+{
+    public class Palindrome
+    {
+        public string IsPalindrome(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return " Invlaid Input ";
+            }
+            else
+            { 
+                char[] to_array = text.ToCharArray();
+                Array.Reverse(to_array);
+                string reversed_text = new string(to_array);
+                if (text.Equals(reversed_text, StringComparison.OrdinalIgnoreCase))
+                {
+                    return "The text is a palindrome.";
+                }
+                else
+                {
+                    return "The text is not a palindrome.";
+                }
+            }
+        }
+    }
+}
---
+++ C#_Practice Problems/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using C__Practice_Problems;
+
+Palindrome pal_obj = new Palindrome();
+Console.WriteLine(pal_obj.IsPalindrome("GADAG"));
